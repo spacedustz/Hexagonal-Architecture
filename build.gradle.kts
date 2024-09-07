2201af72909c039568a6b04eac7d21cbf1ac4865
owen <wavefunctionc@gmail.com>
2024-09-07 21:45:48 +0900

    Root Project gradlew 설정 파일 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,46 +1,78 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.3.3"
-    id("io.spring.dependency-management") version "1.1.6"
-    kotlin("plugin.jpa") version "1.9.25"
+    id(libs.plugins.spring.boot.get().pluginId) version libs.plugins.spring.boot.get().version.toString()
+    id(libs.plugins.spring.dependency.management.get().pluginId) version libs.plugins.spring.dependency.management.get().version.toString()
+    id(libs.plugins.kotlin.jvm.get().pluginId) version libs.plugins.kotlin.jvm.get().version.toString()
+    id(libs.plugins.kotlin.jpa.get().pluginId) version libs.plugins.kotlin.jpa.get().version.toString()
+    id(libs.plugins.kotlin.spring.get().pluginId) version libs.plugins.kotlin.spring.get().version.toString()
 }
 
-group = "com"
-version = "0.0.1"
+allprojects {
+    group = "com.hexagon"
+    version = "0.0.1"
 
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
+    repositories {
+        mavenCentral()
     }
 }
 
-configurations {
-    compileOnly {
-        extendsFrom(configurations.annotationProcessor.get())
+subprojects {
+    apply(plugin = "idea")
+    apply(plugin = "kotlin")
+    apply(plugin = rootProject.libs.plugins.kotlin.jvm.get().pluginId)
+    apply(plugin = rootProject.libs.plugins.kotlin.jpa.get().pluginId)
+    apply(plugin = rootProject.libs.plugins.kotlin.spring.get().pluginId)
+    apply(plugin = rootProject.libs.plugins.kotlin.kapt.get().pluginId)
+    apply(plugin = rootProject.libs.plugins.kotlin.noarg.get().pluginId)
+    apply(plugin = rootProject.libs.plugins.kotlin.allopen.get().pluginId)
+    apply(plugin = rootProject.libs.plugins.spring.boot.get().pluginId)
+    apply(plugin = rootProject.libs.plugins.spring.dependency.management.get().pluginId)
+
+    dependencies {
+        implementation(rootProject.libs.kotlin.stdlib)
+        implementation(rootProject.libs.kotlin.reflect)
+        implementation(rootProject.libs.jackson.module.kotlin)
+
+        annotationProcessor(rootProject.libs.spring.boot.configuration.processor)
+
+        testImplementation(rootProject.libs.mockk)
+        testImplementation(rootProject.libs.spring.mockk)
+        testImplementation(rootProject.libs.kotest.runner.junit5)
+        testImplementation(rootProject.libs.kotest.assertions.core)
+        testImplementation(rootProject.libs.kotest.extensions.spring)
+        testImplementation(rootProject.libs.spring.boot.starter.test)
     }
+
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(rootProject.libs.versions.java.get()))
+        }
+    }
+
+    configurations {
+        compileOnly {
+            extendsFrom(configurations.annotationProcessor.get())
+        }
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs += "-Xjsr305=strict"
+            jvmTarget = rootProject.libs.versions.java.get()
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
 }
 
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    compileOnly("org.projectlombok:lombok")
-    runtimeOnly("com.h2database:h2")
-    annotationProcessor("org.projectlombok:lombok")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-kotlin {
-    compilerOptions {
-        freeCompilerArgs.addAll("-Xjsr305=strict")
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
     }
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,36 @@
-rootProject.name = "hexagonal"
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+
+rootProject.name = "hexagonal-architecture"
+
+// common
+include(":common")
+include(":ulid")
+project(":ulid").projectDir = file("./common/ulid")
+
+// domain
+include("domain")
+
+// usecase
+include("usecase")
+
+// infrastructures
+include(":infrastructure")
+include(":persistence")
+project(":persistence").projectDir = file("./infrastructure/persistence")
+
+// bootstraps
+include(":bootstrap")
+include(":api")
+project(":api").projectDir = file("./bootstrap/api")
+
+include(":worker")
+project(":worker").projectDir = file("./bootstrap/worker")
