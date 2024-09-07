398a64a5e99c74c73422698f3df72c851c11aed8
owen <wavefunctionc@gmail.com>
2024-09-07 23:48:00 +0900

    Ulid Util 추가

---
+++ common/ulid/src/main/kotlin/com/ulid/UlidUtil.kt
@@ -0,0 +1,10 @@
+package com.ulid
+
+import com.github.f4b6a3.ulid.UlidCreator
+import java.util.*
+
+object UlidUtil {
+    fun createUlid(): UUID {
+        return UlidCreator.getMonotonicUlid().toUuid()
+    }
+}
